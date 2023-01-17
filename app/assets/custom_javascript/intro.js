f13b4921c3ddb6a7802c0554c2a06981103ab9b1
Devon VanDyk <devonvandyk@gmail.com>
2023-01-17 13:50:32 -0400

    added multiple sentences to intro, param to typeSentence for reuse with each sentence

--- app/assets/custom_javascript/intro.js
+++ app/assets/custom_javascript/intro.js
@@ -1,13 +1,16 @@
 
 window.onload = () => {
 
-  let sentence = "Welcome to the Med App, this website is a proof-of-concept showing various uses of CRUD and the Model-View-Controller pattern."
-  let i = 0;
+  let sentence_1 = "My name is Devon and I created this site using Ruby on Rails and a sprinkle of Javascript."
+  let sentence_2 = "This website is a proof-of-concept showing various uses of CRUD and the Model-View-Controller pattern."
+  let sentence_3 = "Welcome to the Med App! Click 'Sign Up' to get started or 'Sign In' if you've been here before."
+  
+  let i = -1;
 
-  function typeSentence() {
+  function typeSentence(sentence) {
 
-    sentence_type = setInterval(function(){ 
-
+    const sentence_type = setInterval(function(){ 
+    
     document.getElementById("sentenceloop").innerHTML += sentence[i];
 
     if (i == sentence.length - 1){
@@ -16,16 +19,17 @@ window.onload = () => {
       i++
     }
     
-  }, 60);
+    }, 60);
+    i++
   }
 
   function deleteSentence() {
     
-    sentence_loop = document.getElementById("sentenceloop")
-    sentence_type_two = setInterval(function(){ 
+    const sentence_loop = document.getElementById("sentenceloop")
+    const sentence_delete = setInterval(function(){ 
     
     if (i < 0){
-      clearInterval(sentence_type_two);
+      clearInterval(sentence_delete);
     } else {
       sentence_loop.innerHTML = sentence_loop.innerHTML.slice(0, -1);
       i--
@@ -62,10 +66,12 @@ window.onload = () => {
   }
 
 
-  typeSentence();
+  typeSentence(sentence_1);
   cursorBlink();
 
-  setTimeout(stopCursor, 13000);
-  setTimeout(deleteSentence, 9000);
-
+  setTimeout(deleteSentence, 7000);
+  setTimeout(typeSentence.bind(null, sentence_2), 9000);
+  setTimeout(deleteSentence, 16000);
+  setTimeout(typeSentence.bind(null, sentence_3), 18000);
+  setTimeout(stopCursor, 26000);
 }
